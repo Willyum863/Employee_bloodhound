443337bac1066a9908b4f6570d62e1a8ef9b1aa4
William Combee <william.combee@yahoo.com>
2022-10-05 20:34:19 -0500

    added all employees

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,10 @@ id INTEGER AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INTEGER NOT NULL
+CONSTRAINT fk_department
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -19,5 +23,13 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER 
+    manager_id INTEGER
+     CONSTRAINT fk_role
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+    CONSTRAINT fk_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const connection = require('./db/connections');
 const mysql = require('mysql2');
+const cTable = require("console.table");
 
 //inquirer prompts
 function start() {
@@ -27,7 +28,7 @@ function start() {
           viewAllDepartments();
           break;
         case "View All Roles":
-          viewAllRolls();
+          viewAllRoles();
           break;
         case "View All Employees":
           viewAllEmployees();
@@ -55,20 +56,37 @@ function start() {
 }
 
 //View All Departments
-function viewAllDepartments() {
-  connection.query('SELECT * FROM department', function (err, res){
-    if (err) throw err;
-    console.table('All Departments:', res);
-    start();
+const viewAllDepartments = () => {
+  var sql = `SELECT * FROM department`;
+  connection.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log('All Departments:');
+      console.table(res);
+      start();
   });
 }
 
 //View All Roles
-function viewAllRolls() {
-  connection.query('SELECT role.title, role.id, name AS department FROM role LEFT JOIN department ON role.department_id = department.id, salary', function (err, res) {
-  if (err) throw err;
-  console.table('All Roles:');
-  start();
+const viewAllRoles= () => {
+  var sql = `SELECT * FROM department`;
+  connection.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log('All Roles:');
+      console.table(res);
+      start();
   });
 }
+
+// View All Employees
+const viewAllEmployees = () => {
+  var sql = `SELECT eleft.first_name, eleft.last_name, title, name as department, salary, CONCAT(eright.first_name, ' ', eright.last_name) AS manager FROM employee as eLeft LEFT JOIN role ON eleft.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee eright ON eleft.manager_id = eright.id`;
+  connection.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log('All Employees:');
+      console.table(res);
+      start();
+  });
+}
+
+start();
 
