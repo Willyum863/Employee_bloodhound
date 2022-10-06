de42cfbb392f49022873ca35f878d1f971314c66
William Combee <william.combee@yahoo.com>
2022-10-05 21:59:23 -0500

    added role and employee

--- server.js
+++ server.js
@@ -114,7 +114,87 @@ const addDepartment = () => {
           viewAllDepartments();
       });
   });
-};
+}
+
+//Add Role
+const addRole = () => {
+  connection.query('Select * FROM department', (err, department) => {
+      if (err) throw err;
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What's the new role's title?"
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What's the new role's salary?"
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "What's the new role's department?",
+          choices: myDeps
+        }
+      ])
+      .then(function (data) {
+        connection.query("INSERT INTO role SET ?",
+          {
+            title: data.title,
+            salary: data.salary,
+            department_id: data.department,
+          },
+          function (err, res) {
+            if (err) throw err;
+            viewAllRoles()
+          }
+        );
+      });
+  });
+}
+
+//Add Employee
+const addEmployee = () => {
+  return inquirer.prompt([
+      {
+          type: 'input',
+          name: 'first_name',
+          message: 'What is the first name of the new employee?'
+      },
+      {
+          type: 'input',
+          name: 'last_name',
+          message: 'What is the last name of the new employee?'
+      },
+      {
+          type: 'input',
+          name: 'role_id',
+          message: 'What is the role of the new employee?'
+      },
+      {
+          type: 'input',
+          name: 'manager_id',
+          message: 'What is the employees manager id?'
+      }
+  ])
+  .then(answer => {
+    if (answer.manager_id === 'None') {
+        answer.manager_id = null;
+    };
+    const param = [answer.first_name, answer.last_name, answer.role_id, answer.manager_id];
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES (?, ?, ?, ?)`;
+    
+    connection.query(sql, param, (err, res) => {
+        if (err) throw err;
+        console.log('New employee ' + answer.first_name + ' ' + answer.last_name + ' added.')
+    viewAllEmployees();
+    });
+});
+}
+
+
 
 start();
 
